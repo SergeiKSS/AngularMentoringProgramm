88daee40db9397881b24414cd9cd49ae7fa59bde
Siarhei Karpovich1 <skarpovich@cloudpassage.com>
2019-10-21 00:14:25 +0300

    Added courses list and course component

--- AngularMentoringProgramm/src/app/app.module.ts
+++ AngularMentoringProgramm/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { LogoComponent } from './logo/logo.component';
+import { CoursesComponent } from './courses/courses.component';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    LogoComponent
+    LogoComponent,
+    CoursesComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule,
